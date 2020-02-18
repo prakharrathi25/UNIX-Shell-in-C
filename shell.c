fad55eac51c3bac4c659c2ae48e7ff37da390675
Jaskaran Gujral <jg504@snu.edu.in>
2020-02-18 23:56:21 +0530

    Update shell.c
    Merged

--- shell.c
+++ shell.c
@@ -17,141 +17,142 @@ char *past_com[10]; /* A command to remeber the last 10 commands */
 int main(void)
 {
 
-char *args[MAX_LINE/2 + 1]; /* command line arguments */
-int should_run = 1; /* flag to determine when to exit program */
-while (should_run) {
+    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    int should_run = 1; /* flag to determine when to exit program */
+    while (should_run) {
 
-    char scanned_input[50]; 
-    printf("osh>");
-    fflush(stdout);
-    scanf("%[^\n", scanned_input);
-    getchar();
+        char scanned_input[50]; 
+        printf("osh>");
+        fflush(stdout);
+        scanf("%[^\n", scanned_input);
+        getchar();
 
-    // Take an input and split it into tokens and save them in an array
+        // Take an input and split it into tokens and save them in an array
 
-    // Condition 4
-    if(args[0] == "quit") {
-        exit(0); 
+        // Condition 4
+        if(args[0] == "quit") {
+            exit(0); 
+        }
+
+        else {
+            execvp(args[0], args);
+        }
+
+        /**
+        * After reading user input, the steps are:
+        * (1) fork a child process using fork()
+        * (2) the child process will invoke execvp()
+        * (3) if command included &, parent will not invoke wait()
+        * (4) if command is quit, the shell should exit
+        * Explain your steps as comments in the code itself.
+        */
+
+        return 0;
     }
 
-    else {
-        execvp(args[0], args);
+    // Tokenization function: tokenizes the input stream and separates them on " "
+    char *tokenize(char *string, bool ch)
+    {
+        // Obtain the first token which is the command 
+        char *token = strtok(string, " "); 
+        int i = 0;
+        while(token != NULL) // parsing for all the other tokens
+        {
+            printf("%s\n", token); // printing each token
+            args[i] = token; 
+            i++; 
+
+            token = strtok(NULL, " ");   // EXPLANATION: @Jaskaran idk why this line is working. Please figure out.
+        }
+
+        // Print the command
+        printf("The command you entered is: %s", string[0]); 
+        
+        // Return argument list 
+        return *args;
     }
 
-    /**
-    * After reading user input, the steps are:
-    * (1) fork a child process using fork()
-    * (2) the child process will invoke execvp()
-    * (3) if command included &, parent will not invoke wait()
-    * (4) if command is quit, the shell should exit
-    * Explain your steps as comments in the code itself.
-    */
-
-    return 0;
-}
-
-// Tokenization function: tokenizes the input stream and separates them on " "
-char *tokenize(char *string, bool ch)
-{
-    // Obtain the first token which is the command 
-    char *token = strtok(string, " "); 
-    int i = 0;
-    while(token != NULL) // parsing for all the other tokens
+    // Check for an ampersand
+    int checkAmpersand(char string[MAX_LINE/2 + 1])
     {
-        printf("%s\n", token); // printing each token
-        args[i] = token; 
-        i++; 
-
-        token = strtok(NULL, " ");   // EXPLANATION: @Jaskaran idk why this line is working. Please figure out.
+        char last_arg[50];  
+        // 1. Parse to the last argument
+        // 2. Parse to the last char of the last arg or check for an ampersand anywhere ( might be easier)
+        // 3. If its an ampersand then return 1 else 0 
     }
 
-    // Print the command
-    printf("The command you entered is: %s", string[0]); 
-    
-    // Return argument list 
-    return *args;
-}
-
-// Check for an ampersand
-int checkAmpersand(char string[MAX_LINE/2 + 1])
-{
-    char last_arg[50];  
-    // 1. Parse to the last argument
-    // 2. Parse to the last char of the last arg or check for an ampersand anywhere ( might be easier)
-    // 3. If its an ampersand then return 1 else 0 
-}
-
-// Execute vp command
-int execvp(char *command, char *params[])
-{
-
-}
-
-// Display history 
-void showHistory()
-{
-    // Creating a linked list which adds the latest command to the start 
-    int i = 1;
-
-    // Printing the list of top 10 items
-    while(int crt != NULL & i <= 10)
+    // Execute vp command
+    int execvp(char *command, char *params[])
     {
-        printf("%s", crt->data); 
-        crt = crt->next; 
-        i++; 
+
     }
-}
 
-void executeMostRecent(int n)
-{
-    if(n == 1)
+    // Display history 
+    void showHistory()
     {
-        command = strcat(crt->data, "()")
-    }else
+        // Creating a linked list which adds the latest command to the start 
+        int i = 1;
 
-}
-
-// Fork fucntion 
-int fork(int argc, char *argv[])
-{
-    // Printing the proces id
-    printf("I am a %d\n", (int)getpid()); 
-
-    pid_t pid = fork(); 
-    if (pid == 0)
-    {
-        printf("\nI am a child with pid %d", int getpid())
-    } else if(pid> 0)
-    {
-        printf("\nI am a parent"); 
-    } else
-    {
-        printf("\nFork Failed!"); 
+        // Printing the list of top 10 items
+        while(crt != NULL & i <= 10)
+        {
+            printf("%s", crt->data); 
+            crt = crt->next; 
+            i++; 
+        }
     }
-    print("\nFork returned: %d", int(pid))
-    printf("\nI am a %d\n", (int)getpid())
-    return 0; 
-}
 
-// Function to print Current Directory. 
-void printCurrDir() 
-{ 
-    char cwd[1024]; 
-    getcwd(cwd, sizeof(cwd)); //get current working directory
-    printf("\nDir: %s", cwd); 
-} 
+    void executeMostRecent(int n)
+    {
+        if(n == 1)
+        {
+            command = strcat(crt->data, "()")
+        }else
 
-// Help Function in the Shell
-void openHelp()
-{
-    puts("\n***WELCOME TO OUR SHELL HELP***"
-        "\nCreated by @ Jaskaran and Prakhar"
-        "\nList of Commands supported:"
-        "\n>cd"
-        "\n>ls"
-        "\n>quit"
-        "\ncat"
-        "\n>improper space handling");
+    }
 
-    return;
+    // Fork fucntion 
+    // int fork(int argc, char *argv[])
+    // {
+    //     // Printing the proces id
+    //     printf("I am a %d\n", (int)getpid()); 
+
+    //     pid_t pid = fork(); 
+    //     if (pid == 0)
+    //     {
+    //         printf("\nI am a child with pid %d", int getpid())
+    //     } else if(pid> 0)
+    //     {
+    //         printf("\nI am a parent"); 
+    //     } else
+    //     {
+    //         printf("\nFork Failed!"); 
+    //     }
+    //     print("\nFork returned: %d", int(pid))
+    //     printf("\nI am a %d\n", (int)getpid())
+    //     return 0; 
+    // }
+
+    // Function to print Current Directory. 
+    void printCurrDir() 
+    { 
+        char cwd[1024]; 
+        getcwd(cwd, sizeof(cwd)); //get current working directory
+        printf("\nDir: %s", cwd); 
+    } 
+
+    // Help Function in the Shell
+    void openHelp()
+    {
+        puts("\n***WELCOME TO OUR SHELL HELP***"
+            "\nCreated by @ Jaskaran and Prakhar"
+            "\nList of Commands supported:"
+            "\n>cd"
+            "\n>ls"
+            "\n>quit"
+            "\ncat"
+            "\n>improper space handling");
+
+        return;
+    }
 }
