99b97768360a8082e331aede32b093c97f845c61
Jaskaran Gujral <jg504@snu.edu.in>
2020-02-19 22:04:26 +0530

    Update shell.c
    1) Tokenize finalised.
    2) Tokenize will return true if & is present at the end.
    3) Implicit declaration of functions sorted by shifting the main function to the bottom.
    4) Created an extra array of pointers to store the tokens in main.

--- shell.c
+++ shell.c
@@ -15,77 +15,10 @@ char *args[MAX_LINE/2 + 1]; /* command line arguments */
 
 // Stack for saving the last 10 commands
 int stackPtr = 0; 
-char *past_com[10]; 
-
-// Main Function
-int main(void)
-{
-
-    int should_run = 1; /* flag to determine when to exit program */
-    while (should_run) {
-
-        char scanned_input[50];
-        printf("osh>");
-        fflush(stdout);
-        scanf("%[^\n", scanned_input); // Command + Arguments input stream 
-        getchar();
-
-        // Variable to check if there is an ampersand at the end
-        bool has_ampersand; 
-
-        // Split the input into tokens and save them in an array
-        tokenize(scanned_input, &has_ampersand); // @jaskaran Second argument is a place holder. add whatever is required
-
-        // Creating the child process id and forking the parent process
-        pid_t child_pid; 
-        child_pid = fork(); 
-
-        // If quit statement is found
-        if(args[0] == "quit") 
-        {
-            exit(0);
-        }
-
-        // Checking for the history feature 
-        if(args[0] == "history")
-        {
-            showHistory(); 
-
-        } else if (args[0][0] == '!')
-        {
-            if(args[0][1] == '!') // Executing the most recent command
-                executeRecent(1); 
-            else
-                executeRecent(args[0][1]); // Executing the nth most recent command 
-        } else if(child_pid == 0) 
-        {
-            execvp(args[0], args);
-            fprintf (stderr,"an error occured in execvp\n");
-			abort();
-        }
-
-        // If an ampersand is found then 
-        if(has_ampersand == true && child_pid != 0)
-        {
-            wait(); 
-        }
-
-        /**
-        * After reading user input, the steps are:
-        * (1) fork a child process using fork()
-        * (2) the child process will invoke execvp()
-        * (3) if command included &, parent will not invoke wait()
-        * (4) if command is quit, the shell should exit
-        * Explain your steps as comments in the code itself.
-        */
-
-        return 0;
-    }
-}
-
+char *past_com[10];
 
 // Tokenization function: tokenizes the input stream and separates them on " "
-void tokenize(char *string, char **ret) {
+bool tokenize(char *string, char **ret) {
 
     // Obtain the first token which is the command 
     char *token;
@@ -94,13 +27,19 @@ void tokenize(char *string, char **ret) {
    
     while (token != NULL) // parsing for all the other tokens
     {
-        printf ("%s\n", token); // printing each token
         args[i++] = token;
         token = strtok (NULL, " ");
     }
 
     // Print the command
-    printf("The command you entered is: %s", string[0]); 
+    printf("The command you entered is: %s", string[0]);
+    --i;
+    int j = strlen(ret[i]) - 1;
+    if(ret[i][j] == '&')
+        return true;
+    else
+        return false;
+    
 }
 
 // Display history
@@ -145,4 +84,72 @@ int getSize(char *a)
 {
     int size = sizeof(a)/(sizeof(a[0])); 
     return size; 
+}
+
+// Main Function
+int main(void)
+{
+
+    int should_run = 1; /* flag to determine when to exit program */
+    while (should_run) {
+
+        char scanned_input[50];
+        printf("osh>");
+        fflush(stdout);
+        scanf("%[^\n", scanned_input); // Command + Arguments input stream 
+        getchar();
+
+        // Variable to check if there is an ampersand at the end
+        bool has_ampersand;
+
+        char *arr[50]; // Array of pointers to store tokens
+
+        // Split the input into tokens and save them in an array
+        has_ampersand = tokenize(scanned_input, arr); // Tokenize will return true if & is present
+
+        // Creating the child process id and forking the parent process
+        pid_t child_pid;
+        child_pid = fork();
+
+        // If quit statement is found
+        if(args[0] == "quit")
+        {
+            exit(0);
+        }
+
+        // Checking for the history feature 
+        if(args[0] == "history")
+        {
+            showHistory(); 
+
+        } else if (args[0][0] == '!')
+        {
+            if(args[0][1] == '!') // Executing the most recent command
+                executeRecent(1); 
+            else
+                executeRecent(args[0][1]); // Executing the nth most recent command 
+        } else if(child_pid == 0) 
+        {
+            execvp(args[0], args);
+            fprintf (stderr,"an error occured in execvp\n");
+			abort();
+        }
+
+        // If an ampersand is found then 
+        if(has_ampersand == true && child_pid != 0)
+        {
+            wait(); 
+        }
+
+        /**
+        * After reading user input, the steps are:
+        * (1) fork a child process using fork()
+        * (2) the child process will invoke execvp()
+        * (3) if command included &, parent will not invoke wait()
+        * (4) if command is quit, the shell should exit
+        * Explain your steps as comments in the code itself.
+        */
+
+        return 0;
+    }
 }
