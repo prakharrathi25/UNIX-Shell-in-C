26396a57ec021cd5fbaa411b509a58776d762bda
Prakhar Rathi <prakharrathi25@gmail.com>
2020-02-20 01:46:41 +0530

    Create reshell.c

---
+++ reshell.c
@@ -0,0 +1,53 @@
+/* Name: Prakhar Rathi
+ID:
+Teammate name(s): Jaskaran Gujral
+*/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+// CONSTANTS
+#define MAX_LINE 80 /* The maximum length command */
+char *args[MAX_LINE/2 + 1]; /* command line arguments */
+
+// Stack for saving the last 10 commands
+int stackPtr = 0; 
+char past_com[10][MAX_LINE];
+int main(){
+    int should_run = 1; /* flag to determine when to exit program*/
+    int arg_no = 0; 
+    while (should_run)
+    {
+        int i = 0; 
+        char input[MAX_LINE]; 
+        char *arg; 
+        printf("osh>"); 
+
+        scanf("%[^\n]", input); 
+        getchar(); 
+        bool has_ampersand = 0;
+
+        if(strcmp(input, "history") == 0)
+        {
+            /* Do something */ 
+        }
+        if(input[0] == '!')
+        {
+            /* Do Something */ 
+        } else
+        {
+            add_into_array(input); 
+        }
+
+        pid_t child_pid; 
+        child_pid = fork(); 
+        if((strcmp(input, "quit")) == 0)
+        {
+            exit(0); 
+        }
+    }
+
+}
